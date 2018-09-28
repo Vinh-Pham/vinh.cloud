aecfb74865e998fa9b2eab658169c4cd18a14a95
Vinh Pham <vinh@vinh.cloud>
2018-09-27 23:09:42 -0500

    Remove commented out html5 player

--- src/views/Psyduck.vue
+++ src/views/Psyduck.vue
@@ -8,7 +8,6 @@
 </style>
 
 <template lang="pug">
-  //- video.video-player(ref="videoPlayer", src="https://s3.amazonaws.com/vinh.cloud/Psyduck+Compilation.mp4", controls, preload="auto")
   vue-plyr.video-player(:options="playerOptions")
     video(src="https://s3.amazonaws.com/vinh.cloud/Psyduck+Compilation.mp4", preload="auto")
 </template>
