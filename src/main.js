66f0b4015423747bc2ff24f0b7753a5ed4a0326e
Vinh Pham <vinh@vinh.cloud>
2018-11-25 22:48:07 -0600

    Update packages

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'vue-plyr'
+import VuePlyr from 'vue-plyr'
 import 'vue-plyr/dist/vue-plyr.css'
 
 import '@/style/theme'
-
 Vue.config.productionTip = false
+
+Vue.use(VuePlyr)
 
 new Vue({
   router,
